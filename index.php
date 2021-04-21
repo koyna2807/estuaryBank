1d055569b42f3e03b2de025600c96b683cbe4ea4
koyna2807 <koynasec28@gmail.com>
2021-04-21 20:43:35 +0530

    Final

--- index.php
+++ index.php
@@ -1,14 +1,16 @@
+<!-- 
+	NAME: KOYNA SHARMA
+	TASK: BASIC BANKING SYSTEM
+	GRIP APRIL 2021
+	THE SPARKS FOUNDATION
+ -->
+
+
+
 <!DOCTYPE html>
 <html>
 <head>
 	<title></title>
-	<!-- Required meta tags -->
-    <!-- <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1"> -->
-
-    <!-- Bootstrap CSS -->
-    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous"> -->
-
 	<link rel="stylesheet" type="text/css" href="css.css">
 </head>
 <body>
@@ -20,10 +22,7 @@
 		<h1 style="color: snow; margin-top: -20px; font-size: 40px; text-align: center; font-family: sans-serif;">
 			ESTUARY BANK
 		</h1>
-	</p><!-- 
-	<div id="bank-img">
-			<img style="width: 80%; height: 80%; opacity: 0.3;" src="images/bank.jpg">
-		</div> -->
+	</p>
 	<div class="container">
 		
 		<div id="index-btns" class="col-auto">
@@ -35,8 +34,6 @@
 	</div>
 
 
-
-	<!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script> -->
 	<footer style="margin-bottom: -10px; margin: auto; background-color: white; margin-top: 22vh; text-align: center; font-family: sans-serif;">
 		Developed by Koyna Sharma 
 	</footer>
